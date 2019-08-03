3a0f1238b4d70cf7f0f701efcd1b11050e08fd1b
“Makoto87” <makosinhori@gmail.com>
2019-08-03 16:58:19 +0900

    first Commit

---
+++ MarubasuApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MarubasuApp
+//
+//  Created by VERTEX20 on 2019/08/03.
+//  Copyright © 2019 VERTEX20. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
